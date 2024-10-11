5fd7241c98677d5a1fc4493ab882bf0c2e8b9474
quanghuy2208 <2208huyquang@gmail.com>
2024-10-11 15:52:54 +0700

    Create Editor.vue

---
+++ src/views/forms/Editor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Editor Component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Editor"
+}
+</script>
+
+<style scoped>
+/* Styles for Editor component */
+</style>
